e59b3f598ec3312923dc5cedac26872f0c6d3ec1
Bichi Kim <bichi@live.co.kr>
2019-05-08 10:15:14 +0900

    delete

--- src/boot/axios.ts
+++
@@ -1,5 +0,0 @@
-import axios from 'axios'
-
-export default ({Vue}: any) => {
-  Vue.prototype.$axios = axios
-}
--- src/lib/type.ts
+++
@@ -1,9 +0,0 @@
-import Vue, {ComponentOptions} from 'vue'
-import Router from 'vue-router'
-import {Store} from 'vuex'
-
-export interface Context<V extends Vue, S> {
-  app: ComponentOptions<V>
-  store: Store<S>
-  router: Router
-}
